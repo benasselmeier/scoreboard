6c25433e597bebd26173c3931bd2ec2893dcffb8
Ben <benasselmeier@gmail.com>
2021-07-13 23:42:46 -0500

    sounds differ by event. separated into folders. refactored scoreboard. it sucks less now.

--- Scoreboard.py
+++ Scoreboard.py
@@ -21,62 +21,67 @@ class Scoreboard():
     yellowScore = 0
     blackWins = 0
     yellowWins = 0
+
     def goalSound():
-        sounds = os.listdir('./Sounds/')
-        soundToPlay = str('./Sounds/' + random.choice(sounds))
+        sounds = os.listdir('./Sounds/Goal/')
+        soundToPlay = str('./Sounds/Goal/' + random.choice(sounds))
         playsound(soundToPlay)
+
+    def gameWinSound():
+        sounds = os.listdir('./Sounds/Gamewin/')
+        soundToPlay = str('./Sounds/Gamewin/' + random.choice(sounds))
+        playsound(soundToPlay)
+
+    def matchWinSound():
+        sounds = os.listdir('./Sounds/Matchwin/')
+        soundToPlay = str('./Sounds/Matchwin/' + random.choice(sounds))
+        playsound(soundToPlay)
+
+    def funModeSound():
+        sounds = os.listdir('./Sounds/Funmode/')
+        soundToPlay = str('./Sounds/Funmode/' + random.choice(sounds))
+        playsound(soundToPlay)
+    
 
 
     while blackWins < 2 and yellowWins < 2:
         if blackScore == 6 or yellowScore == 6:
             print("Game Point!")
-        testValue = input("Jank testing suite - input 'y' for yellow goals and 'b' for black goals. \n")
+        if blackScore > 0 or yellowScore > 0:
+            print ("Game Score:\nBlack Team: " + str(blackScore) + "\nYellow Team: " + str(yellowScore))
+
+        testValue = input("Input 'y' for yellow goals and 'b' for black goals. \n")
+
         if testValue == "b":
             #sensorBlack.wait_for_motion()
             print("Black team scored!")
             blackScore = blackScore + 1
-            print ("Game Score:\nBlack Team: " + str(blackScore) + "\nYellow Team: " + str(yellowScore))
             goalSound()
-        if blackScore == 7:
-            print("Black team wins game " + str(currentGame) + "!")
-            blackWins = blackWins + 1
-            if blackWins == 2:
-                print("Black team wins the match!")
-            else:
-                print("Match Score: \n"
-                "Black team: " + str(blackWins) + "\n"
-                "Yellow team: " + str(yellowWins) + "\n"
-                )
-                currentGame = currentGame + 1
+            if blackScore == 7:
                 blackScore = 0
                 yellowScore = 0
+                blackWins = blackWins + 1
+                if blackWins == 2:
+                    print("Black team wins the match!")
+                    matchWinSound()
+                else:
+                    print("Black team wins game " + str(currentGame) + "!")
+                    currentGame = currentGame + 1
+                    gameWinSound()
+
         if testValue == "y":
             #sensorYellow.wait_for_motion()
             print("Yellow team scored!")
             yellowScore = yellowScore + 1
-            print ("Game Score:\nBlack Team: " + str(blackScore) + "\nYellow Team: " + str(yellowScore))
             goalSound()
-        if yellowScore == 7:
-            print("Yellow Team wins game " + str(currentGame) + "!")
-            yellowWins = yellowWins + 1
-            if yellowWins == 2:
-                print("Yellow team wins the match!")
-            else:
-                print("Match Score: \n"
-                "Black team: " + str(blackWins) + "\n"
-                "Yellow team: " + str(yellowWins) + "\n"
-                )
-                currentGame = currentGame + 1
+            if yellowScore == 7:
                 blackScore = 0
                 yellowScore = 0
-
-@app.route('/')
-def index():
-
-    yellowScore
-    blackScore
-    currentGame
-    yellowWins
-    blackWins
-
-    return render_template(yellowScore, blackScore, currentGame, yellowWins, blackWins, 'index.html', title = "Scoreboard")
+                yellowWins = yellowWins + 1
+                if yellowWins == 2:
+                    print("Yellow team wins the match!")
+                    matchWinSound()
+                else:
+                    print("Yellow team wins game " + str(currentGame) + "!")
+                    currentGame = currentGame + 1
+                    gameWinSound()
