00cde0511f273bb11b66a6b7929a14c42a58257f
Ben <benasselmeier@gmail.com>
2021-07-26 20:09:35 -0500

    i believe my iq to be roughly that of a single nike shox tennis shoe

--- Scoreboard.py
+++ Scoreboard.py
@@ -1,83 +1,60 @@
-import gpiozero
-from gpiozero import MotionSensor
-from playsound import playsound
-import os
-import random
+# import gpiozero
+# from gpiozero import MotionSensor
+# from playsound import playsound
+# import os
+# import random
+from threading import current_thread
+from flask import Flask, redirect, render_template
 
+app = Flask(__name__)
+app.config['DEBUG'] = True
 class Scoreboard():
 
-    print(
-            '''
-            Scoreboard started! Get to foosin'!
-            '''
-        )
-
-
-    currentGame = 1
+    currentGame = 0
     blackScore = 0
     yellowScore = 0
     blackWins = 0
     yellowWins = 0
-    sensorBlack = MotionSensor(4)
-    sensorYellow = MotionSensor(17)
 
-    def goalSound():
-        sounds = os.listdir('./Sounds/Goal/')
-        soundToPlay = str('./Sounds/Goal/' + random.choice(sounds))
-        print(soundToPlay)
-        playsound(soundToPlay)
+    # sensorBlack = MotionSensor(4)
+    # sensorYellow = MotionSensor(17)
 
-    def gameWinSound():
-        sounds = os.listdir('./Sounds/Gamewin/')
-        soundToPlay = str('./Sounds/Gamewin/' + random.choice(sounds))
-        playsound(soundToPlay)
-
-    def matchWinSound():
-        sounds = os.listdir('./Sounds/Matchwin/')
-        soundToPlay = str('./Sounds/Matchwin/' + random.choice(sounds))
-        playsound(soundToPlay)
-
-    def funModeSound():
-        sounds = os.listdir('./Sounds/Funmode/')
-        soundToPlay = str('./Sounds/Funmode/' + random.choice(sounds))
-        playsound(soundToPlay)
+    def __init__(self):
+        print(
+        '''
+        Scoreboard ready. \n
+        Default mode.
+        '''
+    )
 
 
 
-    while blackWins < 2 and yellowWins < 2:
-        if blackScore == 6 or yellowScore == 6:
-            print("Game Point!")
-        if blackScore > 0 or yellowScore > 0:
-            print ("Game Score:\nBlack Team: " + str(blackScore) + "\nYellow Team: " + str(yellowScore))
+scoreboard=Scoreboard()
 
-        sensorBlack.wait_for_motion()
-        print("Black team scored!")
-        blackScore = blackScore + 1
-        goalSound()
-        if blackScore == 7:
-            blackScore = 0
-            yellowScore = 0
-            blackWins = blackWins + 1
-            if blackWins == 2:
-                print("Black team wins the match!")
-                matchWinSound()
-            else:
-                print("Black team wins game " + str(currentGame) + "!")
-                currentGame = currentGame + 1
-                gameWinSound()
 
-        sensorYellow.wait_for_motion()
-        print("Yellow team scored!")
-        yellowScore = yellowScore + 1
-        goalSound()
-        if yellowScore == 7:
-            blackScore = 0
-            yellowScore = 0
-            yellowWins = yellowWins + 1
-            if yellowWins == 2:
-                print("Yellow team wins the match!")
-                matchWinSound()
-            else:
-                print("Yellow team wins game " + str(currentGame) + "!")
-                currentGame = currentGame + 1
-                gameWinSound()
+        
+
+@app.route('/')
+def newGame():
+    
+    scoreboard.currentGame = 1
+    print(scoreboard.currentGame)
+    scoreboard.blackScore = 0
+    scoreboard.yellowScore = 0
+
+    print(str(scoreboard.currentGame) + ' < cg yw > ' + str(scoreboard.yellowWins))
+
+
+
+    return render_template('/scoreboard.html', blackScore = scoreboard.blackScore)
+
+@app.route('/goal')
+def goal():
+    blackScore = scoreboard.blackScore + 1
+    return
+
+    
+    
+
+if __name__ == '__main__':
+    app.run()
