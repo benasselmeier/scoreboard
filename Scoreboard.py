af99f5a1c8e9876ab04842a022a26efe3807946e
Ben Asselmeier <benjamin.asselmeier@vizientinc.com>
2021-08-12 16:17:55 -0500

    more work that will probably end up reverted

--- Scoreboard.py
+++ Scoreboard.py
@@ -1,13 +1,13 @@
 # import gpiozero
 # from gpiozero import MotionSensor
+from flask.scaffold import F
 from playsound import playsound
 import os
 import random
 from self import self
-from flask import Flask, redirect, render_template, request
+# from flask import Flask, redirect, render_template, request
 
-app = Flask(__name__)
-app.config['DEBUG'] = True
+# app = Flask(__name__)
 
 class Scoreboard():
 
@@ -16,7 +16,6 @@ class Scoreboard():
             Scoreboard started! Get to foosin'!
             '''
         )
-
     def __init__(self):
         self.currentGame = 1
         self.teamAScore = 0
@@ -28,7 +27,6 @@ class Scoreboard():
         # self.sensorBlack = MotionSensor(4)
         # self.sensorYellow = MotionSensor(17)
 
-
     def display(self):
             print(
                     '|Team A----Foosball----Team B|\n'
@@ -37,13 +35,14 @@ class Scoreboard():
                     '|Wins:'+str(self.teamAWins)+'---|----------|-Wins:'+str(self.teamBWins)+'|\n'
                     '|-----------Game '+str(self.currentGame)+'-----------|\n'
                 )
-            return render_template('scoreboard.html',
-            teamASide = self.teamASide,
-            teamBSide = self.teamBSide,
-            teamAScore = self.teamAScore,
-            teamBScore = self.teamBScore,
-            currentGame = self.currentGame
-            )
+            # return render_template('scoreboard.html',
+            #     teamASide = self.teamASide,
+            #     teamBSide = self.teamBSide,
+            #     teamAScore = self.teamAScore,
+            #     teamBScore = self.teamBScore,
+            #     teamAWins = self.teamAWins,
+            #     teamBWins = self.teamBWins,
+            #     currentGame = self.currentGame)
 
     def goalSound(self):
         sounds = os.listdir('./Sounds/Goal/')
@@ -73,6 +72,7 @@ class Scoreboard():
         else:
             teamBScore = self.teamBScore + 1
             setattr(self, 'teamBScore', teamBScore)
+
     def yellowGoal(self):
         print('Yellow team goal!')
         if(self.teamASide == 'Yellow'):
@@ -128,31 +128,30 @@ while scoreboard.teamAWins < 2 and scoreboard.teamBWins < 2:
     bob = input('y or b ')
     # sensorBlack.wait_for_motion()
     if(bob == 'b'):
-        #scoreboard.goalSound()
+        scoreboard.goalSound()
         scoreboard.blackGoal()
         if scoreboard.teamAScore == 7:
             if scoreboard.teamAWins == 2:
                 print(scoreboard.teamASide + ' wins the match!')
-                # scoreboard.matchWinSound()
+                scoreboard.matchWinSound()
             else: scoreboard.newGame('Black')
         elif scoreboard.teamBScore == 7:
             if scoreboard.teamBWins == 2:
                 print(scoreboard.teamBSide + ' wins the match!')
-                # scoreboard.gameWinSound()
+                scoreboard.gameWinSound()
             else: scoreboard.newGame('Black')
     if(bob == 'y'):
-        # scoreboard.goalSound()
+        scoreboard.goalSound()
         scoreboard.yellowGoal()
         if scoreboard.teamAScore == 7:
             if scoreboard.teamAWins == 2:
                 print(scoreboard.teamASide + ' wins the match!')
-                # scoreboard.matchWinSound()
+                scoreboard.matchWinSound()
             else: scoreboard.newGame('Yellow')
         elif scoreboard.teamBScore == 7:
             if scoreboard.teamBWins == 2:
                 print(scoreboard.teamBSide + ' wins the match!')
-                # scoreboard.gameWinSound()
+                scoreboard.gameWinSound()
             else: scoreboard.newGame('Black')
 
-if __name__ == '__main__':
-    app.run()
+# app.run()
