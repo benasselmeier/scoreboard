454cb105bbdb16fbf9d7042fc67a27b97ce66b99
Asselmeier <BENJAMIN.ASSELMEIER@VIZIENTINC.COM>
2021-08-17 13:16:30 -0500

    completed a decent refactor and some cleanup

--- Scoreboard.py
+++ Scoreboard.py
@@ -1,3 +1,6 @@
+import os
+import random
+
 class Scoreboard():
 
     def __init__(self):
@@ -12,17 +15,73 @@ class Scoreboard():
         # self.sensorYellow = MotionSensor(17)
 
     def display(self):
-            print(
-                    '|Team A----Foosball----Team B|\n'
-                    '|'+ self.teamASide + '                 ' + self.teamBSide +'|\n'
-                    '|Pts:'+str(self.teamAScore)+'----|----------|--Pts:'+str(self.teamBScore)+'|\n'
-                    '|Wins:'+str(self.teamAWins)+'---|----------|-Wins:'+str(self.teamBWins)+'|\n'
-                    '|-----------Game '+str(self.currentGame)+'-----------|\n'
+        print(
+              '|Team A----Foosball----Team B|\n'
+              '|'+ self.teamASide + '                 ' + self.teamBSide +'|\n'
+              '|Pts:'+str(self.teamAScore)+'----|----------|--Pts:'+str(self.teamBScore)+'|\n'
+              '|Wins:'+str(self.teamAWins)+'---|----------|-Wins:'+str(self.teamBWins)+'|\n'
+              '|-----------Game '+str(self.currentGame)+'-----------|\n'
                 )
 
+        return self.currentGame, self.teamAScore, self.teamBScore, self.teamAWins, self.teamBWins, self.teamASide, self.teamBSide
+
+    def addGoal(self, team):
+        print (team + ' team scored!')
+        if(team == 'Black'):
+            if(self.teamASide == team):
+                setattr(self, 'teamAScore', self.teamAScore + 1)
+            if(self.teamBSide == team):
+                setattr(self, 'teamBScore', self.teamBScore + 1)
+        if(team == 'Yellow'):
+            if(self.teamASide == team):
+                setattr(self, 'teamAScore', self.teamAScore + 1)
+            if(self.teamBSide == team):
+                setattr(self, 'teamBScore', self.teamBScore + 1)
+
+    def playSoundEffect(self, type):
+        print
+        if(type != ''):
+            sounds = os.listdir('./Sounds/' + type + '/')
+            soundToPlay = str('/Sounds/' + type + '/' + random.choice(sounds))
+        else:
+            print("Something went wrong when trying to play a sound...")
 
 
-    def winner(self, team):
+    def checkGameWin(self):
+        if(self.teamAScore == 7):
+            teamAWins = self.teamAWins + 1
+            setattr(self, 'teamAWins', teamAWins)
+            self.checkMatchWin()
+            return self.newGame()
+        if(self.teamBScore == 7):
+            teamBWins = self.teamBWins + 1
+            setattr(self, 'teamBWins', teamBWins)
+            self.checkMatchWin()
+            return self.newGame()
+
+    def checkMatchWin(self):
+        if(self.teamAWins == 2):
+            return self.declareWinner(self.teamASide)
+        if(self.teamBWins == 2):
+            return self.declareWinner(self.teamBSide)
+
+        
+    def newGame(self):
+        currentGame = self.currentGame + 1
+        if(self.teamAWins < 2 and self.teamBWins < 2):
+            print('Game ' + str(currentGame) + ' starting. Switch sides!')
+            setattr(self, 'teamAScore', 0)
+            setattr(self, 'teamBScore', 0)
+            setattr(self, 'currentGame', currentGame)
+            if(self.currentGame == 1 or self.currentGame == 3):
+                setattr(self, 'teamASide', 'Black')
+                setattr(self, 'teamBSide', 'Yellow')
+            if(self.currentGame == 2):
+                setattr(self, 'teamASide', 'Yellow')
+                setattr(self, 'teamBSide', 'Black')
+
+
+    def declareWinner(self, team):
         if team == 'Black':
             print('''
                   ____  _            _      _____                     __        ___           _
@@ -40,60 +99,3 @@ class Scoreboard():
                    | |  __/ | | (_) \ V  V /    | |  __/ (_| | | | | | |   \ V  V / | | | | \__ \_|
                    |_|\___|_|_|\___/ \_/\_/     |_|\___|\__,_|_| |_| |_|    \_/\_/  |_|_| |_|___(_)
             ''')
-
-    def playSoundEffect(self, type):
-        if(type != null):
-            sounds = os.listdir('./Sounds/' + type + '/')
-            soundToPlay = str('/Sounds/' + type + '/' + random.choice(sounds))
-        else:
-            print("Something went wrong when trying to play a sound...")
-
-    def blackGoal(self):
-        print('Black team goal!')
-        if(self.teamASide == 'Black'):
-            setattr(self, 'teamAScore', self.teamAScore + 1)
-        else:
-            teamBScore = self.teamBScore + 1
-            setattr(self, 'teamBScore', teamBScore)
-
-    def yellowGoal(self):
-        print('Yellow team goal!')
-        if(self.teamASide == 'Yellow'):
-            teamAScore = self.teamAScore + 1
-            setattr(self, 'teamAScore', teamAScore)
-        else:
-            teamBScore = self.teamBScore + 1
-            setattr(self,'teamBScore', teamBScore)
-
-    def newGame(self, team):
-        if(team == 'Black'):
-            if(self.teamASide == team):
-                teamAWins = scoreboard.teamAWins + 1
-                setattr(self, 'teamAWins', teamAWins)
-            if(self.teamBSide == team):
-                teamBWins = scoreboard.teamBWins + 1
-                setattr(self,'teamBWins', teamBWins)
-        if(team == 'Yellow'):
-            if(self.teamASide == team):
-                teamAWins = scoreboard.teamAWins + 1
-                setattr(self, 'teamAWins', teamAWins)
-            if(self.teamBSide == team):
-                teamBWins = scoreboard.teamBWins + 1
-                setattr(self,'teamBWins', teamBWins)
-        currentGame = self.currentGame + 1
-        if(self.teamAWins < 2 and self.teamBWins < 2):
-            print('Game ' + str(currentGame) + ' starting. Switch sides!')
-        if(self.teamAWins == 2):
-            return scoreboard.winner(self.teamASide)
-        if(self.teamBWins == 2):
-            return scoreboard.winner(self.teamBSide)
-        else:
-            setattr(self, 'teamAScore', 0)
-            setattr(self, 'teamBScore', 0)
-            setattr(self, 'currentGame', currentGame)
-            if(self.currentGame == 1 or self.currentGame == 3):
-                setattr(self, 'teamASide', 'Black')
-                setattr(self, 'teamBSide', 'Yellow')
-            if(self.currentGame == 2):
-                setattr(self, 'teamASide', 'Yellow')
-                setattr(self, 'teamBSide', 'Black')
--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 # import gpiozero
 # from gpiozero import MotionSensor
-from flask.templating import render_template_string
 # from playsound import playsound
 import os
 import random
 # from self import self
 from flask import Flask, redirect, render_template, request, render_template_string
-import Scoreboard
+from Scoreboard import Scoreboard
 
 app = Flask(__name__)
 
 
 scoreboard = Scoreboard()
 
-@app.route('/')
-def renderView():
+def index():
+
+    scoreboard.display()
+
     return render_template('index.html',
     teamAScore = scoreboard.teamAScore,
     teamBScore = scoreboard.teamBScore,
@@ -25,55 +26,29 @@ def renderView():
     teamBSide = scoreboard.teamBSide,
     )
 
-
-    # while scoreboard.teamAWins < 2 and scoreboard.teamBWins < 2:
-    #     if scoreboard.teamAScore == 6 or scoreboard.teamBScore == 6:
-    #         print("Game Point!")
-    #     if (scoreboard.teamAScore > 0 or scoreboard.teamBScore > 0):
-    #         if(scoreboard.teamAScore < 7 or scoreboard.teamBScore < 7):
-                # scoreboard.display()
-                # return render_template('index.html',
-                # teamAScore = scoreboard.teamAScore,
-                # teamBScore = scoreboard.teamBScore,
-                # teamAWins = scoreboard.teamAWins,
-                # teamBWins = scoreboard.teamBWins,
-                # currentGame = scoreboard.currentGame,
-                # teamASide = scoreboard.teamASide,
-                # teamBSide = scoreboard.teamBSide,
-                # )
-def doTheScoringThing(bob):
-    # sensorBlack.wait_for_motion()
-    if(bob == 'b'):
-        # scoreboard.goalSound()
-        scoreboard.blackGoal()
-        if scoreboard.teamAScore == 7:
-            if scoreboard.teamAWins == 2:
-                print(scoreboard.teamASide + ' wins the match!')
-                # scoreboard.matchWinSound()
-            else: scoreboard.newGame('Black')
-        elif scoreboard.teamBScore == 7:
-            if scoreboard.teamBWins == 2:
-                print(scoreboard.teamBSide + ' wins the match!')
-                # scoreboard.gameWinSound()
-            else: scoreboard.newGame('Black')
-    if(bob == 'y'):
-        # scoreboard.goalSound()
-        scoreboard.yellowGoal()
-        if scoreboard.teamAScore == 7:
-            if scoreboard.teamAWins == 2:
-                scoreboard.winner('Yellow')
-                # scoreboard.matchWinSound()
-            else: scoreboard.newGame('Yellow')
-        elif scoreboard.teamBScore == 7:
-            if scoreboard.teamBWins == 2:
-                scoreboard.winner('Black')
-                # scoreboard.gameWinSound()
-            else: scoreboard.newGame('Black')
-
-while scoreboard.teamAWins < 2 and scoreboard.teamBWins < 2:
-    if(scoreboard.teamAScore < 7 or scoreboard.teamBScore < 7):
+def scoreMatch():
+    while scoreboard.teamAWins < 2 and scoreboard.teamBWins < 2:
         scoreboard.display()
-        bob = input('y or b')
-        doTheScoringThing(bob)
+        waitForGoal()
+        
+
+def waitForGoal():
+    goalScored = input('input y or b for a goal')
+    if(goalScored == 'b'):
+        scoringTeam = 'Black'
+        scoreboard.addGoal(scoringTeam)
+        scoreboard.playSoundEffect('Goal')
+        scoreboard.checkGameWin()
+        scoreboard.checkMatchWin()
+    return render_template('index.html',
+    teamAScore = scoreboard.teamAScore,
+    teamBScore = scoreboard.teamBScore,
+    teamAWins = scoreboard.teamAWins,
+    teamBWins = scoreboard.teamBWins,
+    currentGame = scoreboard.currentGame,
+    teamASide = scoreboard.teamASide,
+    teamBSide = scoreboard.teamBSide,
+    )
+
 
 app.run()
