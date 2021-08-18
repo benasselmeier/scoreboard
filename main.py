5d803559a7112c1541aedeec33e6902075f82964
Ben Asselmeier <benasselmeier@gmail.com>
2021-08-18 10:52:45 -0500

    removed flask stuff for now

--- main.py
+++ main.py
@@ -15,61 +15,19 @@ GPIO.add_event_detect(4, GPIO.BOTH)
 
 sensorBlackPin = 4
 
-
-
-while True:
-    if GPIO.input(sensorBlackPin):
-        # scoringTeam = 'Black'
-        # scoreboard.addGoal(scoringTeam)
-        # scoreboard.playSoundEffect('Goal')
-        # scoreboard.checkGameWin()
-        # scoreboard.checkMatchWin()
-        print('beam unbroken')
-    else: print('beam broken!')
-    time.sleep(0.5)
-
-
 scoreboard = Scoreboard()
 
-# @app.route('/')
-# def render():
-#     return render_template('index.html',
-#     teamAScore = scoreboard.teamAScore,
-#     teamBScore = scoreboard.teamBScore,
-#     teamAWins = scoreboard.teamAWins,
-#     teamBWins = scoreboard.teamBWins,
-#     currentGame = scoreboard.currentGame,
-#     teamASide = scoreboard.teamASide,
-#     teamBSide = scoreboard.teamBSide,
-#     )
 
-        
+while scoreboard.teamAWins < 2 and scoreboard.teamBWins < 2:
+    if GPIO.input(sensorBlackPin):
+        print ('Waiting for goal...')
+    else:
+        scoreboard.addGoal('Black')
+        scoreboard.playSoundEffect('Goal')
+        scoreboard.checkGameWin()
+        scoreboard.checkMatchWin()
+        scoreboard.display()
+    time.sleep(0.5)
 
-# def waitForGoal():
-#     goalScored = input('input y or b for a goal')
-#     if(goalScored == 'b'):
-#         scoringTeam = 'Black'
-#         scoreboard.addGoal(scoringTeam)
-#         scoreboard.playSoundEffect('Goal')
-#         scoreboard.checkGameWin()
-#         scoreboard.checkMatchWin()
-#     return render_template('index.html',
-#     teamAScore = scoreboard.teamAScore,
-#     teamBScore = scoreboard.teamBScore,
-#     teamAWins = scoreboard.teamAWins,
-#     teamBWins = scoreboard.teamBWins,
-#     currentGame = scoreboard.currentGame,
-#     teamASide = scoreboard.teamASide,
-#     teamBSide = scoreboard.teamBSide,
-#     )
-
-
-# if scoreboard.teamAWins < 2 and scoreboard.teamBWins < 2:
-#     scoreboard.display()
-#     waitForGoal()
-#     render()
-# else:
-#     scoreboard.declareWinner('Black')
-#     render_template('winner.html')
 
 # app.run()
